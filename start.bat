4257a405f41c0391c6082dc0bbe463c4d61ac045
NogyangSpigot <commit@approximasterstudios2004.ddnsgeek.com>
2025-01-25 15:37:36 +0000

    save

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@ echo off
 cls
 
 :start
-java -jar NogyangSpigotCraftBukkit-paperclip-1.21.4-R0.1-SNAPSHOT-reobf.jar
+java -jar -Xms6G -Xmx7G NogyangSpigotCraftBukkit-paperclip-1.21.4-R0.1-SNAPSHOT-reobf.jar
 goto restart
 
 :restart
